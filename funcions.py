bb513d5e777a5625f5f8286465e4b2071f468c76
Ushaswarsyamala <ushachinnimudhuru01@gmail.com>
2025-08-20 17:35:08 +0530

    fghjk

---
+++ funcions.py
@@ -0,0 +1,56 @@
+# def class_test(a,b):
+#     sum = a+b
+#     print(sum)
+#     return(sum)
+# class_test(1,2)
+# def avg_func(a,b,c,d):
+#     avg = (a+b+c+d)/4
+#     print(int(avg))
+#     return avg
+# avg_func(5,6,9,8)
+#using seperators(space and /n(default))
+# print("hlo usha", end=" ")
+# print ("how are you?")
+#print("hlo usha", end="\n")
+#print ("how are you?")
+
+# #WAf to print length of the lists
+# L1=["usha", "anand", "kanha", "Radhe"]
+# L2=["hare","rama","Siya","lakshmi"]
+
+# def len_fun(list):
+#     print(len(list))
+# len_fun(L2)
+#waf to calculate factorial of n
+# n= int (input("enter number:", ))
+# def cal_factfun(n):
+#     fact=1
+#     for i in range(1,n+1):
+#         fact *=i
+#     print(fact)
+       
+# cal_factfun(n)
+#waf to convert input value as usd to INR
+# n= int(input("enter USD:" ))
+# def converter_func(n):
+#     INR=n*83
+#     print(n,"USD=",INR)
+# converter_func(n)
+#waf to take inout and give output as string "even"if input is even and "odd" if input is odd
+# n=int(input("enter number"))
+# def oddeven_func(n):
+#     if n%2==0:
+#         print("even")
+#     else:
+#         print("odd")
+# oddeven_func(n)
+
+def fun():
+    x=10
+    print(x)        
+    return x
+op = fun()
+print(op)
+
+
+    
